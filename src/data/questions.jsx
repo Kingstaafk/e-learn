ff7a692d48960af52cdd1cdc359368c7a4212d93
Kingstaafk <83715216+Kingstaafk@users.noreply.github.com>
2024-11-26 13:05:54 +0530

    hi

---
+++ src/data/questions.jsx
@@ -0,0 +1,9 @@
+export const questions = [
+  { id: 1, question: "What is your name?", answer: "john" },
+  { id: 2, question: "How are you?", answer: "iamfinethankyou" },
+  {
+    id: 3,
+    question: "What is the capital of France?",
+    answer: "paris",
+  },
+];
